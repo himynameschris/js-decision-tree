99c9fa656174d15892802a7092e24d8abb7015fa
Chris Hannon <chris@channon.us>
2018-01-01 22:46:16 -0500

    moves data to dedicated folder
    Signed-off-by: Chris Hannon <chris@channon.us>

--- src/dtree.js
+++ src/dtree.js
@@ -7,15 +7,17 @@ var rendered_leaves = [];
 
 // build our tree hash-map
 var buildTree = function(leaf) {
-    console.log('building leaf: ', leaf.leafID);
-    if(leaf.end) return;
+    //console.log('build tree adding leaf: ', leaf);
+    //if(leaf.end) return;
+    leaf.leafID = leaf.leafID.replace(/\./g,'d');
+    leaf.parent = leaf.parent.replace(/\./g,'d');
     tree[leaf.leafID] = leaf;
-    if(leaf.children) {
-        leaf.children.map(bud => buildTree(bud));
-    }
+    if(leaf.parent != '0') {
+        if(tree[leaf.parent].children) tree[leaf.parent].children.push(leaf)
+        else tree[leaf.parent].children = [leaf];
+    } 
     return;    
 }
-buildTree(config);
 
 // grow a leaf by assembling the HTML snippet, adding it to the tree and fading in
 var growLeaf = function(leaf) {
@@ -27,20 +29,21 @@ var growLeaf = function(leaf) {
                         '<h3><a href="#" onclick="handleClick(\'' + leaf.leafID + '\')">' + leaf.title + '</a></h3>' +
                         '<div>' + leaf.text + '</div>' + 
                         '<ul>';
-
-    for(var i = 0; i < leaf.children.length; i++) {
-        leafTemplate += '<li>'
-        if(leaf.children[i].end) {
-            // when link on an end node is clicked, take the action
-            leafTemplate += '<a href="' + leaf.children[i].action + '" target="_blank">' + leaf.children[i].choice + '</a>';
-        } else {
-            // when a link on a choice is clicked, handle based on position
-            leafTemplate += '<a href="#" onclick="handleClick(\'' + leaf.children[i].leafID + '\',true)">' + leaf.children[i].choice + '</a>';
+    if(leaf.children) {
+        for(var i = 0; i < leaf.children.length; i++) {
+            leafTemplate += '<li>'
+            if(leaf.children[i].action) {
+                // when link on an end node is clicked, take the action
+                leafTemplate += '<a href="' + leaf.children[i].action + '" target="_blank">' + leaf.children[i].choiceText + '</a>';
+            } else {
+                // when a link on a choice is clicked, handle based on position
+                leafTemplate += '<a href="#" onclick="handleClick(\'' + leaf.children[i].leafID + '\',true)">' + leaf.children[i].choiceText + '</a>';
+            }
+            leafTemplate += '</li>';
         }
-        leafTemplate += '</li>';
-    }
+    }    
 
-    leafTemplate += '</ul></p></div>'
+    leafTemplate += '</ul></p></div>';
 
     $('#root').prepend(leafTemplate);
     rendered_leaves.push(leaf.leafID);
@@ -78,6 +81,7 @@ var witherLeaf = function(nbr, cb) {
 
 // handle a click on a link within a leaf
 var handleClick = function(leafID, isChoice) {
+    //console.log('handling click from leafID:',leafID);
     
     // calculate the level based on the leafID
     var level = ('' + leafID).split('d').length;
@@ -101,9 +105,20 @@ var handleClick = function(leafID, isChoice) {
 
 $(document).ready(function() {
     
-    growLeaf(config);
+    Papa.parse('http://localhost:9000/data/data.csv', 
+        {
+            download:true
+            ,header:true
+            ,complete:function(results) {
+                console.log('papaparse results: ', results);
+                var leaves = results.data;
 
-    console.log('tree: ', tree);
-    console.log('rendered leaves: ', rendered_leaves);
+                leaves.map(leaf => buildTree(leaf));
+                //buildTree(leaves);
+                growLeaf(tree['1']);
+                console.log('tree: ', tree);
+                console.log('rendered leaves: ', rendered_leaves);
+            }
+        });
 
 });

f00dea36ee36b5189bac7808020bbd100b54556d
Hannon <channon@opers.org>
2018-01-02 14:37:13 -0500

    fix multiple loads of dtree.js, clean up codes, fix handling of parent for first node
    Signed-off-by: Hannon <channon@opers.org>

--- src/dtree.js
+++ src/dtree.js
@@ -1,14 +1,25 @@
+// define our namespace
+var dtree = dtree || {};
+
+// helper to keep from being loaded multiple times
+var isFirstLoad = function(namesp) {
+    var isFirst = namesp.firstLoad === undefined;
+    namesp.firstLoad = false;
+    return isFirst;
+}
+
 // a hash-map tree to reference nodes by their ID
 var tree = {};
-var dtree_level = 0;
+// a pile to keep track of our current leaves
 var rendered_leaves = [];
 
-// build our tree hash-map
+// add a leaf to our tree hash-map for lookup later
 var buildTree = function(leaf) {
+    if(!leaf.leafID) return;
     leaf.leafID = leaf.leafID.replace(/\./g,'d');
-    leaf.parent = leaf.parent.replace(/\./g,'d');
     tree[leaf.leafID] = leaf;
     if(leaf.parent != '0') {
+        leaf.parent = leaf.parent.replace(/\./g,'d');
         if(tree[leaf.parent].children) tree[leaf.parent].children.push(leaf)
         else tree[leaf.parent].children = [leaf];
     } 
@@ -21,7 +32,7 @@ var growLeaf = function(leaf) {
     // assemble our template with the leaf info
     var leafTemplate = '<div id="' + leaf.leafID + '" class="leaf">' +
                         // when title link is clicked, remove all nodes after
-                        '<h3><a href="#" onclick="handleClick(\'' + leaf.leafID + '\')">' + leaf.title + '</a></h3>' +
+                        '<a href="#" onclick="handleClick(\'' + leaf.leafID + '\')">' + leaf.title + '</a>' +
                         '<div>' + leaf.text + '</div>' + 
                         '<ul>';
     if(leaf.children) {
@@ -40,27 +51,28 @@ var growLeaf = function(leaf) {
 
     leafTemplate += '</ul></div>';
 
+    // add the leaf to the DOM
     $('#root').prepend(leafTemplate);
+    // add the leaf to our pile
     rendered_leaves.push(leaf.leafID);
+    // style the leaf
     $("#" + leaf.leafID).css({'transform' : 'rotate('+ (4 - (Math.random() * 8).toFixed(0)) +'deg)'});
     if(rendered_leaves.length % 2 == 0) {
         $("#" + leaf.leafID).css({'background' : '#cfc'});
     } else if (rendered_leaves.length % 3 == 0) {
         $("#" + leaf.leafID).css({'background' : '#ccf'});
     }
-    
+    // make the leaf visible
     $("#" + leaf.leafID).fadeIn('fast');
-
-    dtree_level++;
 }
 
+// remove a leaf or leaves from the DOM, fade out
 var witherLeaf = function(nbr, cb) {
 
     function wither(nbr) {
         var remove_id = "#" + rendered_leaves.pop();
         $(remove_id).fadeOut("fast", function() {
             $(remove_id).remove();
-            dtree_level--;
             nbr--;
             if(nbr > 0) {
                 wither();
@@ -79,14 +91,14 @@ var handleClick = function(leafID, isChoice) {
     
     // calculate the level based on the leafID
     var level = ('' + leafID).split('d').length;
-    if(level > dtree_level) {
+    if(level > rendered_leaves.length) {
         // leaf is ahead of current lead, add
         growLeaf(tree[leafID]);
 
-    } else if (level <= dtree_level) {
+    } else if (level <= rendered_leaves.length) {
         // leaf is below current leaf, remove all nodes below
         if(leafID == rendered_leaves[rendered_leaves.length - 1]) return;
-        var ceiling = dtree_level - level;
+        var ceiling = rendered_leaves.length - level;
         if(isChoice) ceiling++;
         witherLeaf(ceiling, function() {
             if(isChoice) {
@@ -96,21 +108,25 @@ var handleClick = function(leafID, isChoice) {
     }
 }
 
-
+// load our data and render our tree
 $(document).ready(function() {
-    
+    // check that this is the first load
+    if(!isFirstLoad(dtree)) {
+        return;
+    }
+
+    // parse our data
     Papa.parse(dataurl, 
         {
             download:true
             ,header:true
-            ,complete:function(results) {
+            ,complete: function(results) 
+            {
                 var leaves = results.data;
-
                 leaves.map(function(leaf) {
                     buildTree(leaf);
                 })
                 growLeaf(tree['1']);
             }
-        });
-
+        })
 });
